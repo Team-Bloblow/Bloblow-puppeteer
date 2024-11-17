3ce96c6b798c433c62f584f0c689b636fd70a6ff
Ssong-Q <skdmlrh12@gmail.com>
2024-11-17 11:27:02 +0900

    Fix: timeout으로 인한 코드 수정

--- controllers/crawlController.js
+++ controllers/crawlController.js
@@ -12,48 +12,59 @@ const crawlPostData = async (req, res) => {
       "--no-sandbox",
       "--disable-setuid-sandbox",
       "--disable-dev-shm-usage",
-      "--single-process",
     ],
     ignoreHTTPSErrors: true,
     defaultViewport: {
       width: 1080, height: 1024,
     }
   });
+
+  const TIMEOUT = 100000;
+
   try {
     const page = await browser.newPage();
     console.log("Page created");
 
-    await page.setDefaultNavigationTimeout(100000);
+    await page.setDefaultNavigationTimeout(TIMEOUT);
 
     await page.setUserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36");
-
     await page.setRequestInterception(true);
+    
     page.on("request", (req) => {
-      const blockResources = ["image", "stylesheet", "font"];
+      const blockResources = ["image", "stylesheet", "font", "media", "other"];
       if (blockResources.includes(req.resourceType())) req.abort();
       else req.continue();
     });
 
     console.log(`Navigated to ${decodedLink}`);
-    await page.goto(decodedLink, { waitUntil: "networkidle2", timeout: 60000 });
+    await page.goto(decodedLink, { waitUntil: "networkidle2", timeout: TIMEOUT });
 
-    await page.waitForSelector("iframe", { timeout: 100000 });
+    await page.waitForSelector("iframe", { timeout: TIMEOUT });
 
     const iframeURL = await page.evaluate(() => document.querySelector("iframe").src);
 
-    console.log(`Navigating to iframe URL: ${iframeURL}`);
-    await page.goto(iframeURL, { waitUntil: "networkidle2", timeout: 60000 });
-    await page.waitForNetworkIdle({ timeout: 100000 });
+    if (!iframeURL || !iframeURL.startsWith("https://blog.naver.com")) {
+      throw new Error("Invalid iframe URL");
+    }
 
-    const content = await page.evaluate(() =>
-      JSON.stringify(document.querySelector(".se-main-container").textContent)
-    );
-    const commentCount = await page.evaluate(
-      () => parseInt(document.querySelector("._commentCount").innerText.trim()) || 0
-    );
-    const likeCount = await page.evaluate(
-      () => parseInt(document.querySelector(".u_cnt._count").innerText.trim()) || 0
-    );
+    console.log(`Navigating to iframe URL: ${iframeURL}`);
+    await page.goto(iframeURL, { waitUntil: "networkidle2", timeout: TIMEOUT });
+    await page.waitForNetworkIdle({ timeout: TIMEOUT });
+
+    console.log('network status: idle');
+
+    const content = await page.evaluate(() => {
+      const element = document.querySelector(".se-main-container");
+      return element ? element.textContent : "Content not found";
+    });
+    const commentCount = await page.evaluate(() => {
+      const element = document.querySelector("._commentCount");
+      return element ? parseInt(element.innerText.trim()) || 0 : 0;
+    });
+    const likeCount = await page.evaluate(() => {
+      const element = document.querySelector(".u_cnt._count");
+      return element ? parseInt(element.innerText.trim()) || 0 : 0;
+    });
     const isAd = await Promise.resolve(
       validateAdKeyword.some((adKeyword) => content.includes(adKeyword))
     );
@@ -68,7 +79,12 @@ const crawlPostData = async (req, res) => {
     console.error(e);
     return res.status(500).send({ message: `[ServerError] Error occured crawling ${e}` });
   } finally {
-    await browser.close();
+    try {
+      await browser.close();
+      console.log("Browser closed successfully.");
+    } catch (closeError) {
+      console.error("Error closing the browser:", closeError.message);
+    }
   }
 }
 
